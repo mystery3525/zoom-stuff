4e2e932ff22187a8e995e7f5079fa39aeae2e55c
mystery3525 <30960302+mystery3525@users.noreply.github.com>
2025-07-17 23:43:39 -0400

    Added the signalis flaregun to the anti-rpg restrictions

--- lua/autorun/server/anti_rpg_infantry.lua
+++ lua/autorun/server/anti_rpg_infantry.lua
@@ -12,17 +12,17 @@ local function getSwepClass(dmginfo)
 end
 
 hook.Add( "EntityTakeDamage", "NoRPGAgainstInfantry", function( target, dmginfo )
-    if dmginfo:GetDamageType() != DMG_BLAST then
+    if not dmginfo:IsExplosionDamage() then
         return -- first, ignore all non-blast damage. This is the most common indicator so its more efficient to do it first
     end
 
     local attacker = dmginfo:GetAttacker()
     local swep_class = getSwepClass( dmginfo )
-    if not (swep_class == "weapon_rpg" or swep_class == "rpg-7" or swep_class == "glide_homing_launcher") then
+    if not (swep_class == "weapon_rpg" or swep_class == "rpg-7" or swep_class == "glide_homing_launcher" or swep_class == "arc9_uplp_signalis_flaregun") then
         if not (swep_class == "nil" and attacker:Health() > 0) then
             return -- ignore any explosive damage from weapons not listed here
         end -- we ignore this situation if we can't find the weapon and the attacker is dead (health <= 0) because people can just suicide the rpg. I don't think dead people should be doing max explosion damage anyways.
-    end 
+    end
 
     if target == attacker then
         return -- we should do nothing trololo
