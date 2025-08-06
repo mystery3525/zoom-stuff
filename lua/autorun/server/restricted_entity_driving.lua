beb65cd37c6ebb9a67dc0c6e0190dca44eb539bb
mystery3525 <30960302+mystery3525@users.noreply.github.com>
2025-08-05 21:42:56 -0400

    Removed restrictions for spawnpads and rpgs

--- lua/autorun/server/restricted_entity_driving.lua
+++
@@ -1,5 +0,0 @@
-hook.Add( "CanDrive", "AdjustDriveHook", function ( ply, ent )
-    if ent:GetClass() == "personal-spawn-pad" then
-        return false
-    end
-end )
--- lua/autorun/server/restricted_physgun.lua
+++
@@ -1,5 +0,0 @@
-hook.Add( "PhysgunPickup", "ZoomPhysgunHook", function ( ply, ent, phys )
-    if ent:GetClass() == "personal-spawn-pad" then
-        return false
-    end
-end )
